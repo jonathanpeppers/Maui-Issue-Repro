4b4a6186f9b582bf40de2c5286905aa7aa59fe5a
Ethan <ethanellis@adam.com.au>
2023-02-19 18:52:06 +1030

    init

---
+++ MauiAppCollectionViewFooterResize/MauiAppCollectionViewFooterResize/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAppCollectionViewFooterResize;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MauiAppCollectionViewFooterResize/MauiAppCollectionViewFooterResize/ViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiAppCollectionViewFooterResize
+{
+    internal class ViewModel
+    {
+        public ObservableCollection<string> ItemList { get; set; }
+
+        public ObservableCollection<string> ItemListFooter { get; set; }
+
+        public Command AddCommand => new (Add);
+
+        public ViewModel()
+        {
+            ItemList = new ObservableCollection<string>
+            {
+                "Red",
+                "Blue",
+                "Green",
+                "Red",
+                "Blue",
+                "Green",
+                "Red",
+                "Blue",
+                "Green",
+                "Red",
+                "Blue",
+                "Green",
+                "Red",
+                "Blue",
+                "Green",
+            };
+            ItemListFooter = new ObservableCollection<string>
+            {
+                "Paris",
+                "London"
+            };
+        }
+
+        private void Add()
+        {
+            ItemListFooter.Add("Madrid");
+        }
+    }
+}
